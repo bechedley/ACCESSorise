181a839110aa33b2feb4c3482a0b9eb57597ccec
bechedley <109792989+bechedley@users.noreply.github.com>
2023-02-08 05:05:19 +1100

    removed console logs

--- client/src/components/CategoryItem/index.js
+++ client/src/components/CategoryItem/index.js
@@ -42,8 +42,6 @@ const CategoryItem = (props) => {
     });
   };
 
-  console.log(categoryData);
-
 
   return (
     <div className='p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-3'>
